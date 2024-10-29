715d1814d54c15a948e37bdfb0ff70f36b871d10
Quentin VERDIER <quent1.verdier@gmail.com>
2024-10-29 16:09:39 +0100

    🏗️ update Home component to center map on Nantes and update marker popup text

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,14 +3,16 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import styles from './Home.module.css';
 
 function Home() {
+  const nantesCenter: [number, number] = [47.2184, -1.5536];
+
   return (
-    <MapContainer className={styles.mapContainer} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer className={styles.mapContainer} center={nantesCenter} zoom={13} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>A pretty CSS3 popup.</Popup>
+      <Marker position={nantesCenter}>
+        <Popup>Ptdr c'est le centre de Nantes</Popup>
       </Marker>
     </MapContainer>
   );
