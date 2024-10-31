cc0c768539fca01824311208819a071f13c2fdd5
Wexop <mattheurtin@gmail.com>
2024-10-31 10:17:45 +0100

    Stop informations

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,6 +3,7 @@ import { getStopInfo } from '@utils/fetchData';
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import styles from './Home.module.css';
+import L from 'leaflet';
 
 function Home() {
   const stopInfo: TStop[] = getStopInfo();
@@ -16,11 +17,41 @@ function Home() {
       />
       {stopInfo
         .filter((stop) => stop.parent_station === null)
-        .map((stop) => (
-          <Marker key={stop.stop_id} position={[stop.stop_coordinates.lat, stop.stop_coordinates.lon]}>
-            <Popup>{stop.stop_name}</Popup>
-          </Marker>
-        ))}
+        .map((stop) => {
+
+          const findChildren = stopInfo.filter(l => l.parent_station === stop.stop_id);
+          
+
+          return (
+            (
+              <Marker key={stop.stop_id} position={[stop.stop_coordinates.lat, stop.stop_coordinates.lon]}>
+                <Popup >
+                  <div>
+                    <p>{ stop.stop_name }</p>
+                    <p>Lignes disponibles : </p>
+
+                      {findChildren.map(c => {
+                        return (
+                          <div className={ styles.alignCenter }>
+                            <p>{ c.stop_name }</p>;
+                            {
+                              c.wheelchair_boarding && <img className={ styles.handicapIcon }
+                                                            src={ 'https://accessibleicon.org/img/Accessibility%20Icon_final.svg' }
+                                                            alt={ '' } /> }
+                          </div>
+
+
+                        )
+                      })}
+
+                  </div>
+
+
+                </Popup>
+              </Marker>
+        )
+        )
+        } ) }
     </MapContainer>
   );
 }
