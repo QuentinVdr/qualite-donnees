a476fba268f96efc9ad64fa795d5b05ba68983b8
Quentin  VERDIER <quent1.verdier@gmail.com>
2024-11-04 16:20:57 +0100

    ✨ Add startup logging in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import Home from '@components/Home/Home';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    console.log("Démarrage de l'application");
+    console.log(" (\\ (\\ \n ( -.-)\n o_('')('')");
+  }, []);
+
   return <Home />;
 }
 
